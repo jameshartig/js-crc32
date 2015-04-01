c5ba3f8691e760d547a6713890724d289cb9b6d9
James Hartig <james.hartig@grooveshark.com>
2015-04-01 02:19:04 -0400

    Initial Commit

---
+++ crc32.js
@@ -0,0 +1,62 @@
+(function(window, undefined) {
+    var arrayType = Array,
+        CRC32_TABLE;
+    if (typeof Uint32Array === 'function') {
+        arrayType = Uint32Array;
+    }
+
+    function stringToArray(string) {
+        var bytes = new arrayType(string.length),
+            i = 0,
+            l = string.length;
+        for (; i < l; ++i) {
+            bytes[i] = string.charCodeAt(i);
+        }
+        return bytes;
+    }
+
+    function genCRC32Table() {
+        var n = 0, c = 0;
+        CRC32_TABLE = new arrayType(256);
+        for (n = 0; n < 256; n++) {
+            c = n;
+            //unrolled loop for small perf gain (4x?)
+            c = (c & 1) ? ((c >>> 1) ^ 0xEDB88320) : (c >>> 1);
+            c = (c & 1) ? ((c >>> 1) ^ 0xEDB88320) : (c >>> 1);
+            c = (c & 1) ? ((c >>> 1) ^ 0xEDB88320) : (c >>> 1);
+            c = (c & 1) ? ((c >>> 1) ^ 0xEDB88320) : (c >>> 1);
+            c = (c & 1) ? ((c >>> 1) ^ 0xEDB88320) : (c >>> 1);
+            c = (c & 1) ? ((c >>> 1) ^ 0xEDB88320) : (c >>> 1);
+            c = (c & 1) ? ((c >>> 1) ^ 0xEDB88320) : (c >>> 1);
+            c = (c & 1) ? ((c >>> 1) ^ 0xEDB88320) : (c >>> 1);
+            CRC32_TABLE[n] = c;
+        }
+    }
+
+    function crc32(arr) {
+        var values = typeof arr === 'string' ? stringToArray(arr) : arr,
+            crc = -1,
+            i = 0,
+            l = values.length;
+        if (CRC32_TABLE === undefined) {
+            genCRC32Table();
+        }
+        for (; i < l; i++) {
+            crc = CRC32_TABLE[(crc ^ values[i]) & 0xFF] ^ (crc >>> 8);
+        }
+        //bitflip then cast to 32-bit unsigned
+        return (~crc >>> 0).toString(16);
+    }
+
+    if (typeof module !== "undefined") {
+        module.exports = crc32;
+    } else {
+        window.crc32 = crc32;
+
+        if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+            define('crc32', function() {
+                return crc32;
+            });
+        }
+    }
+}(this));
---
+++ tests/test.js
@@ -0,0 +1,15 @@
+var assert = require('assert'),
+    crc32 = require('../crc32.js'),
+    arr;
+
+
+arr = [0, 1, 2, 3];
+assert.equal(crc32(arr), '8bb98613');
+
+arr = [Math.pow(2, 32)];
+assert.equal(crc32(arr), 'd202ef8d');
+
+//compared against https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/mann/crc32.n.html
+assert.equal(crc32('Hello, World!'), 'ec4ac3d0');
+
+assert.equal(crc32('alejandro'), 'aac5a14f');
